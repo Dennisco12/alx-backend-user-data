a6a6b6fc346503a8ee948d6bdb5e100f0a0f97ad
Dennisco12 <akinwonjowodennisco@gmail.com>
2023-03-08 02:51:51 +0000

    Added tasks 0 to 11 in the basic authentication task

---
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""This manages the API authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """class definition
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ This returns false """
+        if path and path[-1] != '/':
+            path += '/'
+
+        if path and path not in excluded_paths:
+            return True
+        if excluded_paths is None or excluded_paths == []:
+            return True
+        if path and path in excluded_paths:
+            return False
+        return False
+
+
+    def authorization_header(self, request=None) -> str:
+        """ This returns None """
+        if request is None:
+            return None
+
+        return request.headers.get('Authorization', None)
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """This returns None"""
+        return None
